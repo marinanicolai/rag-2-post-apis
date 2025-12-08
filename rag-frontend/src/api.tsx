decc43c84fd1abbaa99ac22179673f74590e7d6f
Marina Nicolai <nicolaimarina@gmail.com>
2025-12-08 16:41:44 -0500

    edit the front-end to connectto the backend

--- rag-frontend/src/api.tsx
+++ rag-frontend/src/api.tsx
@@ -1,5 +1,5 @@
 // src/api.ts
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL as string;
 
 export async function uploadFile(file: File) {
   const formData = new FormData();
